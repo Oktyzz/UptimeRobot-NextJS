ea9622f53508123d673bc4c078feb7c4dd3019d0
Oktyzz <realredmin@gmail.com>
2024-08-10 18:37:56 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c95";
+let uptimeRobotToken = "ur2426839-28a004e57d167bf462a3ffa4";
 // let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c5";
 
 export { uptimeRobotToken };
